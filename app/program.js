25b89e7fdce9dca9f1064ad10a8325dc89c8e443
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-22 20:43:11 +0100

    Add function changeTime

--- app/program.js
+++ app/program.js
@@ -22,7 +22,7 @@ process.stdin.on('readable', function() {//nasłuchiwanie na zdarzenie odczytu
                 break;
             
             case '/getOSinfo':
-                OSInfo.print();
+                OSinfo.print();
                 break;
 
             default:
